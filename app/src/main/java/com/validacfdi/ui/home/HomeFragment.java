cc965f1246f5591820650271d8c051af7609e703
Oscar Garcia <oscar_syo@hotmail.com>
2020-05-23 13:04:22 -0500

    mensaje

--- app/src/main/java/com/validacfdi/ui/home/HomeFragment.java
+++ app/src/main/java/com/validacfdi/ui/home/HomeFragment.java
@@ -1,26 +1,65 @@
 package com.validacfdi.ui.home;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.URLUtil;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.validacfdi.R;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.SoapFault;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpResponseException;
+import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+import static android.provider.ContactsContract.CommonDataKinds.Identity.NAMESPACE;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private SurfaceView cameraView;
+    private CameraSource cameraSource;
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private String token = "";
+    private String tokenanterior = "";
+    private Context context;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        cameraView = (SurfaceView) root.findViewById(R.id.camera_view);
+        context = this.getContext();
+
+        /*homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
@@ -29,7 +68,183 @@ public class HomeFragment extends Fragment {
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+
+        //initQR();
+
+        ClienteWS myRequest = new ClienteWS();
+        myRequest.execute();
+
         return root;
     }
+
+    public void initQR() {
+
+        // creo el detector qr
+        BarcodeDetector barcodeDetector =
+                new BarcodeDetector.Builder(context)
+                        .setBarcodeFormats(Barcode.ALL_FORMATS)
+                        .build();
+
+        // creo la camara
+        cameraSource = new CameraSource
+                .Builder(context, barcodeDetector)
+                .setRequestedPreviewSize(1600, 1024)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+
+        // listener de ciclo de vida de la camara
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                // verifico si el usuario dio los permisos para la camara
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        // verificamos la version de ANdroid que sea al menos la M para mostrar
+                        // el dialog de la solicitud de la camara
+                        if (shouldShowRequestPermissionRationale(
+                                Manifest.permission.CAMERA)) ;
+                        requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                MY_PERMISSIONS_REQUEST_CAMERA);
+                    }
+                    return;
+                } else {
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException ie) {
+                        Log.e("CAMERA SOURCE", ie.getMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        // preparo el detector de QR
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() > 0) {
+
+                    // obtenemos el token
+                    token = barcodes.valueAt(0).displayValue.toString();
+
+                    // verificamos que el token anterior no se igual al actual
+                    // esto es util para evitar multiples llamadas empleando el mismo token
+                    if (!token.equals(tokenanterior)) {
+
+                        // guardamos el ultimo token proceado
+                        tokenanterior = token;
+                        Log.i("token", token);
+
+                        if (URLUtil.isValidUrl(token)) {
+                            // si es una URL valida abre el navegador
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(token));
+                            startActivity(browserIntent);
+                        } else {
+                            // comparte en otras apps
+                            Intent shareIntent = new Intent();
+                            shareIntent.setAction(Intent.ACTION_SEND);
+                            shareIntent.putExtra(Intent.EXTRA_TEXT, token);
+                            shareIntent.setType("text/plain");
+                            startActivity(shareIntent);
+                        }
+
+                        new Thread(new Runnable() {
+                            public void run() {
+                                try {
+                                    synchronized (this) {
+                                        wait(5000);
+                                        // limpiamos el token
+                                        tokenanterior = "";
+                                    }
+                                } catch (InterruptedException e) {
+                                    // TODO Auto-generated catch block
+                                    Log.e("Error", "Waiting didnt work!!");
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
+
+                    }
+                }
+            }
+        });
+
+    }
+
+
+    public class ClienteWS extends AsyncTask<String, Integer, String> {
+
+        String URL = "https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc";
+        private static final String SOAP_ACTION = "http://tempuri.org/IConsultaCFDIService/Consulta";
+        private static final String METHOD_NAME = "Consulta";
+        private static final String NAMESPACE = "http://tempuri.org/";
+
+        @Override
+        protected String doInBackground(String... strings) {
+            //for linear parameter
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+            String em = "?re=GIC900115IN7&rr=SUB910603SB3&tt=6046.85&id=5D5774FE-132C-4E52-B3E2-A53FAC6DD69A";
+            request.addProperty("expresionImpresa", em); // adding method property here serially
+            /*request.addProperty("re", "GIC900115IN7"); // adding method property here serially
+            request.addProperty("rr", "SUB910603SB3"); // adding method property here serially
+            request.addProperty("tt", "6046.85"); // adding method property here serially
+            request.addProperty("id", "5D5774FE-132C-4E52-B3E2-A53FAC6DD69A"); // adding method property here serially
+*/
+            SoapSerializationEnvelope envelope = new  SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.implicitTypes = true;
+            envelope.setOutputSoapObject(request);
+            envelope.dotNet = true;
+
+            HttpTransportSE httpTransport = new HttpTransportSE(URL);
+            httpTransport.debug = true;
+
+            try {
+                httpTransport.call(SOAP_ACTION, envelope);
+            } catch (HttpResponseException e) {
+                // TODO Auto-generated catch block
+                Log.e("HTTPLOG", e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                Log.e("IOLOG", e.getMessage());
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                // TODO Auto-generated catch block
+                Log.e("XMLLOG", e.getMessage());
+                e.printStackTrace();
+            } //send request
+
+            Object  result = null;
+            try {
+                result = (Object )envelope.getResponse();
+                Log.i("RESPONSE",String.valueOf(result)); // see output in the console
+            } catch (SoapFault e) {
+                // TODO Auto-generated catch block
+                Log.e("SOAPLOG", e.getMessage());
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+
 }
